742154b971add81c984d19e0e010c250fd910fff
JAY Adhav <101917934+jayadhav07@users.noreply.github.com>
2022-09-01 18:21:34 +0530

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,9 +2,9 @@ const Contact = _ =>{
   return(
    <section>
      <h3>Contact Me</h3>
-     <a href = "https://www.linkedin.com/in/jay-adhav/" target="_blank">LinkedIn</a>
-     <a href = "https://github.com/jayadhav07"target="_blank">Github</a>
-     <a href = "https://www.instagram.com/jay_adhav_07/"target="_blank">Instagram</a>
+     <a href = "https://www.linkedin.com/in/jay-adhav/" target="_blank"  rel="noreferrer">LinkedIn</a>
+     <a href = "https://github.com/jayadhav07"target="_blank"  rel="noreferrer">Github</a>
+     <a href = "https://www.instagram.com/jay_adhav_07/"target="_blank"  rel="noreferrer">Instagram</a>
    </section>
   )
 }
